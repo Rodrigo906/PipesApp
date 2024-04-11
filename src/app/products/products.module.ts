a319c6b3c1ffdc4e6c7a7798f361ff87a52320f5
Rodrigo Calizaya <rcalizayadev@gmail.com>
2024-04-11 09:05:49 -0300

    Finalizacion de modulo 12

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BasicsPageComponent } from './basics-page/basics-page.component';
-import { NumbersPageComponent } from './numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './uncommon-page/uncommon-page.component';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { ShortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+    ToggleCasePipe,
+    CanFlyPipe,
+    ShortByPipe
   ],
   imports: [
     CommonModule,
